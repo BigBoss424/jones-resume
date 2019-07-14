a277e2031db8ce46b5db4fc0de3529a0dafdf781
bigboss424 <ajones061392@gmail.com>
2019-07-14 13:08:57 -0700

    Fixed the download link.

---
+++ download.php
@@ -0,0 +1,16 @@
+<?php 
+	if (isset($_GET['../Aaron-Jones.pdf'])){
+		$file = $_GET['../Aaron-Jones.pdf'];
+		if(file_exists($file) && is_readable($file) && preg_match('/\.pdf$/', $file))
+		{
+			header('Content-Type: application/pdf');
+			header("Content-Disposition: attachment";
+					filename=\"$file\"");
+			readfile($file);
+		}else
+		{
+			header("HTTP/1.0 404 Not Found");
+			echo "<h1>Error 404: File Not Found: <br/><em>$file</em></h1>"
+		}
+	}
+?>
